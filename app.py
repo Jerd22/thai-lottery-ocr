3ab6306fca4cb4e49c7290828075a62e821c4175
Jerd22 <teeo300881@gmail.com>
2024-04-23 13:35:46 +0700

    set frontend and backend
    add index.html

---
+++ app.py
@@ -0,0 +1,81 @@
+from fastapi import FastAPI, Request, File, UploadFile
+from starlette.requests import Request
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+import numpy as np
+import time
+import io
+import cv2
+import pytesseract
+from pytesseract import Output
+import uvicorn
+
+class ImageType(BaseModel):
+ url: str
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+start = time.time()
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+end = time.time()
+
+def GetData(label_no_crop):
+  label_no_crop_gray = cv2.cvtColor(label_no_crop, cv2.COLOR_BGR2GRAY)
+  th, threshold = cv2.threshold(label_no_crop_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)  
+  imag2char = pytesseract.image_to_string(threshold, lang="eng")
+  return imag2char
+
+def FileCrop(url):
+  img = cv2.imread(url)
+  d = pytesseract.image_to_data(img, output_type=Output.DICT)
+
+  n_boxes = len(d['text'])
+  for i in range(n_boxes):
+      if int(float(d['conf'][i])) > 60:
+          (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+          img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+  
+  #ตัวเลขเบอร์
+  img = cv2.rectangle(img, (280, 20), (520, 70), ( 9, 9, 255), 2)
+  label_no_crop=img[20:70,280:520]
+  return label_no_crop
+
+def read_img(img):
+  label_no_crop_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  th, label_no_crop_gray = cv2.threshold(label_no_crop_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)  
+  text = pytesseract.image_to_string(label_no_crop_gray, lang="eng")
+  return(text)
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+ 
+@app.post("/extract_text")
+async def extract_text(request: Request):
+    label = ""
+    if request.method == "POST":
+        form = await request.form()
+        # file = form["upload_file"].file
+        contents = await form["upload_file"].read()
+        image_stream = io.BytesIO(contents)
+        image_stream.seek(0)
+        file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
+        frame = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)     
+        imgs = cv2.rectangle(frame, (280, 20), (520, 70), ( 9, 9, 255), 2)
+        label_no_crop=imgs[20:70,280:520]        
+        label =  read_img(label_no_crop)
+ 
+    return templates.TemplateResponse("index.html", {"request": request, "label": label})
+
+#async def create_upload_file(uploaded_file: UploadFile = File(...)):
+#    file_location = f"files/{uploaded_file.filename}"
+#    with open(file_location, "wb+") as file_object:
+#      shutil.copyfileobj(uploaded_file.file, file_object) 
+#      
+#    sfile = FileCrop(file_location)
+#    label_no = GetData(sfile).replace("\n","").replace(" ","")
+#    return {"no": label_no}
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
