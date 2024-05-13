03d66192cd57cf84dde669ef9738c1103b0bd37c
Chespat Saengtawan <teeo300881@gmail.com>
2024-05-13 07:39:40 +0700

    set ip

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ async def extract_text(request: Request):
 #    return {"no": label_no}
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
